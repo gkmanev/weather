0bfc9ebe46e5b0edbbf4173ecff4e8b531436635
Georgi <georgi.manev@entra.energy>
2024-04-09 13:58:25 +0300

    up

--- weather_data/models.py
+++ weather_data/models.py
@@ -3,6 +3,33 @@ from datetime import datetime, timedelta
 from django.utils import timezone
 from pytz import timezone
 
+
+class CustomManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset()
+    
+    def today(self):
+        today = datetime.now(timezone('Europe/London')).date()
+        tomorrow = today + timedelta(1)
+        today_start = str(today)+'T'+'00:00:00Z'
+        today_end = str(tomorrow)+'T'+'00:00:00Z'
+        return self.get_queryset().filter(timestamp__gt = today_start, timestamp__lt = today_end)
+
+
+    def from_beginning_of_month(self):
+        today = timezone.now().date()
+        beginning_of_month = today.replace(day=1)
+        return self.get_queryset().filter(timestamp__gte=beginning_of_month)
+
+    def from_beginning_of_year(self):
+        today = timezone.now().date()
+        beginning_of_year = today.replace(month=1, day=1)
+        return self.get_queryset().filter(timestamp__gte=beginning_of_year)
+
+
+
+
+
 class TodayManager(models.Manager):
     def get_queryset(self):
         today = datetime.now(timezone('Europe/London')).date()
@@ -21,5 +48,5 @@ class Weather(models.Model):
     uv = models.IntegerField(default=0)
     lat = models.FloatField()
     long = models.FloatField()
-    today = TodayManager()
+    custom_ranges = CustomManager()
     objects = models.Manager()
