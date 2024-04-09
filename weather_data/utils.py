a57619703b4f6bfa0510cc221d09dfae3226c6ba
Georgi <georgi.manev@entra.energy>
2024-04-09 12:22:02 +0300

    up

--- weather_data/utils.py
+++ weather_data/utils.py
@@ -57,7 +57,7 @@ def make_weather_request(date, hour):
 def fill_history_data():
     count = -1
     today = datetime.now()
-    for i in range(24):
+    for i in range(24*30):
         count += 1
         one_hour_before = today - timedelta(hours=count)   
         date_part = one_hour_before.strftime('%Y-%m-%d')
